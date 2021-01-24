bd1460e3938351956ce45050a99d1e6b0b30e953
Cerebralhub <cerebralhubit@gmail.com>
2021-01-24 14:02:57 +0100

    cerebral new site design first backup

---
+++ js/modal.js
@@ -0,0 +1,55 @@
+var modal = document.getElementById('id01','ido2','ido3'); 
+window.onclick = function(event) { 
+    if (event.target == modal) { 
+        modal.style.display = "none"; 
+    } ;
+}; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var modal = document.getElementById('id02','ido1','ido3'); 
+window.onclick = function(event) { 
+    if (event.target == modal) { 
+        modal.style.display = "none"; 
+    } 
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var modal = document.getElementById('ido3'); 
+window.onclick = function(event) { 
+    if (event.target == modal) { 
+        modal.style.display = "none"; 
+    } 
+} 
